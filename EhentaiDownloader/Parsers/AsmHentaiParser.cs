125886bd096f26e777b951494065c64ae2020019
Jinge Zhang <jinge_windows@hotmail.com>
2021-04-21 18:20:11 +1000

    更新了AsmHentai网站的解析器

--- EhentaiDownloader/Parsers/AsmHentaiParser.cs
+++ EhentaiDownloader/Parsers/AsmHentaiParser.cs
@@ -11,6 +11,7 @@ using EhentaiDownloader.Delegates;
 using EhentaiDownloader.Exceptions;
 using EhentaiDownloader.Models;
 using EhentaiDownloader.Parsers;
+using System.Text.RegularExpressions;
 
 namespace EhentaiDownloader.Tools
 {
@@ -20,30 +21,47 @@ namespace EhentaiDownloader.Tools
 
         public async Task<List<ImagePageModel>> FindImagePageUrl(TaskItem taskItem)
         {
-            string url = taskItem.Url;
-            if (url[url.Length - 1] != '#')
-            {
-                url += "#";
-            }
-            string html = await HttpDownloader.DownloadHtmlPage(url);
+            //string url = taskItem.Url;
+            //if (url[url.Length - 1] != '#')
+            //{
+            //    url += "#";
+            //}
+            string html = await HttpDownloader.DownloadHtmlPage(taskItem.Url);
 
             IConfiguration config = Configuration.Default;
             IBrowsingContext context = BrowsingContext.New(config);
             IDocument document = await context.OpenAsync(response => response.Content(html));
-            var result = document.All.Where(m => m.LocalName == "div" && m.ClassName == "preview_thumb");
-            if (result.Count() == 0)
-            {
-                throw new TargetNotFindException("未找到div.preview_thumb元素");
-            }
-            Debug.WriteLine("成功：" + "找到图片网页" + result.Count() + "个");
+            //var result = document.All.Where(m => m.LocalName == "div" && m.ClassName == "preview_thumb");
+            //去页面中寻找Page元素，该元素包含当前漫画的总页数。
+            var result = document.All.Where(m => m.LocalName == "div" && m.ClassName == "pages").FirstOrDefault();
+            string pages = result.QuerySelector("h3").TextContent;
+            Match pageNum = Regex.Match(pages, "[0-9]+");
+            int pageCount = Convert.ToInt32(pageNum.ToString());
 
+            //人工根据漫画页数构造每一页的URL
             List<ImagePageModel> imagePageModels = new List<ImagePageModel>();
-            foreach (var res in result)
+            string imagePageUrlBase = taskItem.Url.Replace("g", "gallery");
+            for(int i = 1; i <= pageCount; i++)
             {
-                string pageUrl = "https://asmhentai.com"+res.QuerySelector("a").GetAttribute("href");
-                imagePageModels.Add(new ImagePageModel(pageUrl, taskItem));
+                string imagePageUrl = Path.Combine(imagePageUrlBase, i.ToString());
+                imagePageModels.Add(new ImagePageModel(imagePageUrl, taskItem));
             }
             return imagePageModels;
+
+
+            //if (result.Count() == 0)
+            //{
+            //    throw new TargetNotFindException("未找到div.preview_thumb元素");
+            //}
+            //Debug.WriteLine("成功：" + "找到图片网页" + result.Count() + "个");
+
+            //List<ImagePageModel> imagePageModels = new List<ImagePageModel>();
+            //foreach (var res in result)
+            //{
+            //    string pageUrl = "https://asmhentai.com"+res.QuerySelector("a").GetAttribute("href");
+            //    imagePageModels.Add(new ImagePageModel(pageUrl, taskItem));
+            //}
+            //return imagePageModels;
             //return imagePageUrls.Select(link => "https://asmhentai.com" + link).ToList();
         }
 
